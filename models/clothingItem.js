31e77c05ef733f7e7e7b0f7799da5a0a61b2c7c5
Daisy Gama <gamadaisy1995@outlook.com>
2026-08-18 10:53:29 -0500

    Fix authentication and clothing item controllers

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -28,7 +28,7 @@ const clothingItemSchema = new mongoose.Schema({
     required: true,
   },
   likes:  {
-      type: mongoose.Schema.Types.ObjectId,
+      type: [mongoose.Schema.Types.ObjectId],
       ref: "user",
       default: [],
     },
