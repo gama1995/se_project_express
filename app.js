3aed4bd434b71e7e79cf9dfeef0dedbad01d9c27
Daisy Gama <gamadaisy1995@outlook.com>
2026-09-25 15:42:44 -0500

    Add test user middleware

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+
+  next();
+});
+
 app.get("/crash-test", () => {
   setTimeout(() => {
     throw new Error("Server will crash now");
